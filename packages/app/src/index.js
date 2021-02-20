323c20092e7005383b220352024db2c6d14723b9
Jhony Vega <jhonyvegacuya24@gmail.com>
2021-02-20 11:59:12 -0500

    feat(App package): Add configuration of react app

---
+++ packages/app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>hola</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('__app__'));
---
+++ packages/app/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+/** @returns {import('webpack').Configuration} */
+module.exports = (_args, args) => {
+  const isProduction = args.mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'assets/js/[contenthash].js',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+              envName: isProduction ? 'production' : 'development',
+            },
+          },
+        },
+      ],
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        minSize: 0,
+        maxInitialRequests: 20,
+        maxAsyncRequests: 20,
+      },
+      runtimeChunk: 'single',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        inject: true,
+      }),
+    ],
+  };
+};
