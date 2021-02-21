83f54aac516ef807a7884a4f88542c7f548f5103
Jhony Vega <jhonyvegacuya24@gmail.com>
2021-02-21 11:58:56 -0500

    feat(model-timer): Create model timer and logic

--- packages/app/src/models/timer/index.js
+++ packages/app/src/models/timer/index.js
@@ -1,3 +1,46 @@
-function timer({ initialSeconds = 0 } = {}) {}
+import { combine, createApi, createEvent, createStore } from 'effector';
+import { format } from 'fecha';
+
+function timer({ initialSeconds = 0 } = {}) {
+  // stores
+  const $running = createStore(false);
+  const $seconds = createStore(initialSeconds);
+  const $runningCounter = combine([$running, $seconds]);
+  const $formatTime = combine($seconds, (seconds) =>
+    format(new Date(seconds * 1000), 'mm:ss')
+  );
+
+  // event actions
+  const decrement = createEvent();
+  const setTotalSeconds = createEvent();
+  const resetCounter = createEvent();
+
+  const runningApi = createApi($running, {
+    onStart: () => true,
+    onStop: () => false,
+  });
+
+  $seconds
+    .on(setTotalSeconds, (_, value) => value)
+    .on(decrement, (value) => value - 1)
+    .reset(resetCounter);
+
+  $runningCounter.watch(([running, seconds]) => {
+    if (seconds > 0 && running) {
+      setTimeout(() => decrement(), 1000);
+    }
+    if (seconds === 0) {
+      runningApi.onStop();
+    }
+  });
+
+  return {
+    $running,
+    $seconds,
+    $formatTime,
+    setTotalSeconds,
+    ...runningApi,
+  };
+}
 
 export default timer();
