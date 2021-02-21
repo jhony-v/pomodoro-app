0030d4b2cec1029aa0f2e80d802cf810f65ac8f2
Jhony Vega <jhonyvegacuya24@gmail.com>
2021-02-21 11:50:44 -0500

    feat(model-ui): Create store and events

--- packages/app/src/models/ui/index.js
+++ packages/app/src/models/ui/index.js
@@ -1,5 +1,22 @@
+import { createEvent, createStore } from 'effector';
+
 function ui() {
-  return {};
+  const $theme = createStore('');
+  const $modal = createStore(false);
+  const changeTheme = createEvent();
+
+  // event actions
+  const modalApi = createApi($modal, {
+    openModal: () => true,
+    closeModal: () => false,
+  });
+  $theme.on(changeTheme, (_, newTheme) => newTheme);
+
+  return {
+    $theme,
+    $modal,
+    ...modalApi,
+  };
 }
 
 export default ui();
