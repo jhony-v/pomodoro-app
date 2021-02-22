8f0100da24937cbd162267ba6a2645376de479b9
Jhony Vega <jhonyvegacuya24@gmail.com>
2021-02-21 19:08:12 -0500

    feat(app): Create RoundAvatar

--- packages/app/src/components/RoundAvatar/index.jsx
+++ packages/app/src/components/RoundAvatar/index.jsx
@@ -1,3 +1,25 @@
 import { styled } from '@pomodoro/design';
 
-export default styled('div', {});
+export default styled('div', {
+  borderRadius: 'full',
+  size: '40px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  variants: {
+    bg: {
+      default: {
+        backgroundColor: 'neutral',
+      },
+      primary: {
+        backgroundColor: 'colorOptionPrimary',
+      },
+      secondary: {
+        backgroundColor: 'colorOptionSecondary',
+      },
+      third: {
+        backgroundColor: 'colorOptionThird',
+      },
+    },
+  },
+});
