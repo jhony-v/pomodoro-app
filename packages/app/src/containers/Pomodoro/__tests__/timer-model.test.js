3caa171700a790cff824dffe418ea6078191f8e7
Jhony Vega <jhonyvegacuya24@gmail.com>
2021-02-25 18:37:00 -0500

    test: Add test at timer-model

--- packages/app/src/containers/Pomodoro/__tests__/timer-model.test.js
+++ packages/app/src/containers/Pomodoro/__tests__/timer-model.test.js
@@ -1,5 +1,33 @@
+import { timer } from '../models';
+
+const initialMinutes = 1;
+const initialSeconds = initialMinutes * 60;
+
 describe('timer-model', () => {
-  it('should ', () => {
-    expect(0).toEqual(0);
+  it('should have initial data in store', () => {
+    expect(timer.$completed.getState()).toBeFalsy();
+    expect(timer.$seconds.getState()).toEqual(initialSeconds);
+    expect(timer.$running.getState()).toBeFalsy();
+    expect(timer.$progressPercentaje.getState()).toEqual(0);
   });
-})
+
+  it('should run and pause timer', () => {
+    timer.onStart();
+    expect(timer.$completed.getState()).toBeFalsy();
+    expect(timer.$seconds.getState()).toBeLessThan(initialSeconds);
+    expect(timer.$running.getState()).toBeTruthy();
+    expect(timer.$progressPercentaje.getState()).toBeGreaterThanOrEqual(0);
+    timer.onPause();
+    expect(timer.$completed.getState()).toBeFalsy();
+    expect(timer.$running.getState()).toBeFalsy();
+    expect(timer.$progressPercentaje.getState()).toBeGreaterThanOrEqual(0);
+  });
+
+  it('should reset timer counter', () => {
+    timer.resetCounter();
+    expect(timer.$completed.getState()).toBeFalsy();
+    expect(timer.$seconds.getState()).toEqual(initialSeconds);
+    expect(timer.$running.getState()).toBeFalsy();
+    expect(timer.$progressPercentaje.getState()).toEqual(0);
+  });
+});
