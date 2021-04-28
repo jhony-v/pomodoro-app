4bb477895bcf9dc914640264efef33d7d11deb7d
jhony-24 <jhonyvegacuya24@gmail.com>
2021-04-28 08:08:26 -0500

    Updates

---
+++ assets/js/1ad3b1a06652f6f4fb59.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_pomodoro_app=self.webpackChunk_pomodoro_app||[]).push([[186],{5186:(t,a,o)=>{"use strict";o.d(a,{FU5:()=>r});var p=o(2898);function r(t){return(0,p.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"}}]})(t)}}}]);
